13a47ad41f2f72042c6bff10152d7200cbbb33a4
Jaren-Bresnick <Jefbresnick@gmail.com>
2025-05-13 14:58:37 -0400

    fixed bugs in expand_polynomial_features

--- Utils/Helper_Functions.py
+++ Utils/Helper_Functions.py
@@ -3,13 +3,13 @@ import numpy as np
 
 # Polynomial Regression
 def expand_polynomial_features(X, degree):
+    feature_indices = range(X.shape[1])
     actual_combinations = []
-    combinations = combinations_with_replacement(X, degree)
-    for combination in combinations:
-        total = 1
-        for value in combination:
-            total *= value
-        actual_combinations.append(total)
-    stacked_features = np.hstack(actual_combinations)
-    
+    combs_replacement = combinations_with_replacement(feature_indices, degree)
+    for combination in combs_replacement:
+        feature = np.ones(X.shape[0])
+        for index in combination:
+            feature *= X[ :, index]
+        actual_combinations.append(feature)
+    stacked_features = np.column_stack(actual_combinations)
     return stacked_features
